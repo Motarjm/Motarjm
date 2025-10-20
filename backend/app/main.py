87768e7e2beb0cb79b998cfe73e151949062c8db
Yousefbahr <yousef.tbahr@gmail.com>
2025-10-21 00:00:34 +0300

    removed pathlib import line

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pathlib import Path
 import os
 from app.routers import translate
 
