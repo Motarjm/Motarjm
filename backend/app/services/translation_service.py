f700821e36c80f2601757c608db877ecb8c6bbbe
Yousefbahr <yousef.tbahr@gmail.com>
2025-11-23 01:13:14 +0200

    used huggingface api to translate

--- backend/app/services/translation_service.py
+++ backend/app/services/translation_service.py
@@ -1,23 +1,40 @@
 import requests
+from app.config.config import *
+from backend.app.config.config import TEMPERATURE
+from prompts import *
 
 def translate_text(text: str, source_lang: str = "en", target_lang: str = "ar") -> str:
-    
-    base_url = "https://api.mymemory.translated.net/get"
-    params = {
-        "q": text,
-        "langpair": f"{source_lang}|{target_lang}"
-    }
-
+    sys_prompt = SYSTEM_PROMPT.format(source_lang = source_lang, target_lang = target_lang)
+    trans_prompt = TRANSLATE_PROMPT.format(source_lang=source_lang,
+                                           target_lang=target_lang,
+                                           source_text=text)
+    messages = [
+        {
+            "role":"system",
+            "content":sys_prompt
+        },
+        {
+            "role":"user",
+            "content":trans_prompt
+        }
+    ]
     try:
-        response = requests.get(base_url, params=params, timeout=10)
-        response.raise_for_status()
-        data = response.json()
+        completion = model.chat.completions.create(
+            messages= messages,
+            temperature=TEMPERATURE
+        )
+
+        # response = requests.get(base_url, params=params, timeout=10)
+        # response.raise_for_status()
+        # data = response.json()
+
     except requests.exceptions.RequestException as e:
         raise Exception(f"Network or HTTP error during translation: {e}")
     except ValueError:
         raise Exception("Failed to parse API response as JSON")
 
-    translated = data.get("responseData", {}).get("translatedText")
+    # translated = data.get("responseData", {}).get("translatedText")
+    translated = completion.choices[0].message.content
     if not translated:
         raise Exception("Translation failed or no text returned from API")
 
