76fc0520857a99ca2ea75fb9b47a8cd187873a0b
Yousefbahr <yousef.tbahr@gmail.com>
2026-01-01 00:44:04 +0200

    added react build dir

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,28 +13,11 @@ app.include_router(translate.router)
 # get absolute path to backend/app folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# go two directories up to root
-STATIC_DIR = os.path.join(BASE_DIR, "..", "..", "frontend", "static")
-TEMPLATES_DIR = os.path.join(BASE_DIR, "..", "..", "frontend" ,"templates")
+# Path to React build (dist) directory
+REACT_BUILD_DIR = os.path.join(BASE_DIR, '..', '..', 'frontend_', 'dist')
 
-# Serve static files
-app.mount("/static", StaticFiles(directory= STATIC_DIR), name="static")
-
-# Load HTML templates
-templates = Jinja2Templates(directory=TEMPLATES_DIR)
-
-# Serve the main frontend page
-@app.get("/", response_class=HTMLResponse)
-async def serve_home(request: Request):
-    return templates.TemplateResponse("Torgman.html", {"request": request})
-
-@app.get("/editing", response_class=HTMLResponse)
-async def editing(request: Request):
-    return templates.TemplateResponse("editing_interface.html", {"request": request})
-
-@app.get("/compare", response_class=HTMLResponse)
-async def compare(request: Request):
-    return templates.TemplateResponse("compare_interface.html", {"request": request})
+# Mount the React build directory to serve static files
+app.mount("/", StaticFiles(directory=REACT_BUILD_DIR, html=True), name="react-app")
 
 # Keep your original root endpoint
 @app.get("/api", tags=["Root"])
