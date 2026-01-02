5cd9cb5172fa250d4fa0752f23b0321cb50e0b0e
Yousefbahr <yousef.tbahr@gmail.com>
2026-01-03 01:57:14 +0200

    relocated get environment api keys code to config.py instead of llms.py

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,17 +1,8 @@
 import os
-from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACE_API_KEY")
-
-# model = InferenceClient(
-#     model=os.getenv("MODEL_NAME"),
-#     api_key=os.getenv("HUGGINGFACE_API_KEY"),
-# )
-
-# TEMPERATURE = float(os.getenv("TEMPERATURE"))
-
-# # only import 'model' and 'TEMPERATURE' objects
-# __all__ = ["model", "TEMPERATURE"]
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
--- backend/app/core/llms.py
+++ backend/app/core/llms.py
@@ -1,15 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACE_API_KEY")
-os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
 
 # GOOGLE MODELS
 google_translator = ChatGoogleGenerativeAI(
