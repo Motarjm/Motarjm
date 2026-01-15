2500288deeba34d24744e370031dcb2aaf921203
Yousefbahr <yousef.tbahr@gmail.com>
2026-01-15 21:25:10 +0200

    added langsmith api keys

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -6,3 +6,12 @@ load_dotenv()
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACE_API_KEY")
 os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING"] = os.getenv("LANGSMITH_TRACING")
+os.environ["LANGSMITH_PROJECT"] = "Turjman"
+os.environ["LANGSMITH_ENDPOINT"] = "https://eu.api.smith.langchain.com"
+
+
+
+
+
