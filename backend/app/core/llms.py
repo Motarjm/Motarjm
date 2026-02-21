f27ca124bb44708089f51c88597be61b243649b0
Yousefbahr <yousef.tbahr@gmail.com>
2026-02-21 14:34:39 +0200

    changed some models and added new ones like grok

--- backend/app/core/llms.py
+++ backend/app/core/llms.py
@@ -2,10 +2,10 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 
-# Translators 
+# Translators
 
 # google
-translator_1 = ChatGoogleGenerativeAI(
+translator_2 = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.01,  # Gemini 3.0+ defaults to 1.0
     max_tokens=1024,
@@ -13,12 +13,18 @@ translator_1 = ChatGoogleGenerativeAI(
     max_retries=0,
     # other params...
 ) 
+
 # openrouter
-translator_2 = ChatOpenAI(
-    model="google/gemma-3-27b-it:free",
+translator_1 = ChatOpenAI(
+    model="deepseek/deepseek-v3.2",
+    # model="qwen/qwen-2.5-72b-instruct",
     base_url="https://openrouter.ai/api/v1",
+    temperature=0.01,  # Gemini 3.0+ defaults to 1.0
     max_tokens = 1024,
-    max_retries=0
+    reasoning = {
+        "effort": "none",
+    }
+    # max_retries=0
 )
 
 # hugging
@@ -26,7 +32,7 @@ translator_3 = ChatHuggingFace( llm=HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V3.2",
     max_new_tokens=1024,
     do_sample=True,
-    temperature=0.3,
+    temperature=0.01,  # Gemini 3.0+ defaults to 1.0
     provider="auto",
 ),
                                     max_retries = 0
@@ -38,23 +44,50 @@ translator_3 = ChatHuggingFace( llm=HuggingFaceEndpoint(
 # Evaluators
 
 # hugging
-evaluator_1 = ChatHuggingFace( llm=HuggingFaceEndpoint(
+evaluator_2 = ChatHuggingFace( llm=HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-7B-Instruct",
     max_new_tokens=1024,
     do_sample=True,
-    temperature=0.3,
+    temperature=0.01,
     provider="auto",
 ),
                                     max_retries = 0
 )
 
+
+
+# evaluator_2 = ChatOpenAI(
+#     model="meta-llama/llama-3.3-70b-instruct:free",
+#     base_url="https://openrouter.ai/api/v1",
+#     max_tokens = 1024,
+#     temperature=0.01,
+#     max_retries=0
+# )
+
 # openrouter
-evaluator_2 = ChatOpenAI(
-    model="mistralai/mistral-7b-instruct:free",
+evaluator_1 = ChatOpenAI(
+    # model="google/gemini-2.5-flash-lite",
+    model="deepseek/deepseek-v3.2",
     base_url="https://openrouter.ai/api/v1",
-    max_tokens = 1024,
-    max_retries=0
+    max_tokens = 2048,
+    temperature=0.01,
+    reasoning = {
+        "effort": "none",
+    }
+    # max_retries=0
 )
+
+gemini_2_5_flash_lite = ChatOpenAI(
+    model="google/gemini-2.5-flash-lite",
+    base_url="https://openrouter.ai/api/v1",
+    max_tokens = 2048,
+    temperature=0.01,
+    reasoning = {
+        "effort": "none",
+    }
+    # max_retries=0
+)
+
 # google
 evaluator_3 = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -65,14 +98,20 @@ evaluator_3 = ChatGoogleGenerativeAI(
     # other params...
 ) 
 
+
+
 # Advisors
 
 # openrouter
 advisor_1 = ChatOpenAI(
-    model="deepseek/deepseek-r1-0528:free",
+    model="x-ai/grok-4.1-fast",
     base_url="https://openrouter.ai/api/v1",
     max_tokens = 1024,
-    max_retries=0
+    # max_retries=0,
+    temperature=0.6,  # Gemini 3.0+ defaults to 1.0,
+    reasoning = {
+        "effort": "none",
+    }
 )
 
 
@@ -82,7 +121,7 @@ advisor_2 = advisor_1
 # google
 advisor_3 = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
-    temperature=0.01,  # Gemini 3.0+ defaults to 1.0
+    temperature=0.6,  # Gemini 3.0+ defaults to 1.0
     max_tokens=1024,
     timeout=None,
     max_retries=0,
@@ -100,7 +139,15 @@ providers = {"translator": [translator_1,
              
              "advisor": [advisor_1,
                           advisor_2,
-                          advisor_3]
+                          advisor_3],
+             
+             "terminology": [translator_1],
+             "explanator": [gemini_2_5_flash_lite],
+             
+             "suggestions1": [gemini_2_5_flash_lite],
+             "suggestions2": [advisor_1], # grok
+             "suggestions3": [evaluator_1], # deepseek
+
              }
 
  
