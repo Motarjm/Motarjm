1e8d24f11cc1a8dd693288536f93529e41a91ff4
Yousefbahr <yousef.tbahr@gmail.com>
2026-02-26 12:46:52 +0200

    modified api url for testing

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,5 @@
 // src/apiConfig.js
 
-// export const API_URL = 'http://localhost:8000';
+export const API_URL = 'http://localhost:8000';
 // export const API_URL = 'https://cosmoid-francis-barbarously.ngrok-free.dev';
-export const API_URL = 'https://dimensions-column-dollar-color.trycloudflare.com';
+// export const API_URL = 'https://dimensions-column-dollar-color.trycloudflare.com';
