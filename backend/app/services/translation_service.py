edc8044232137e928529c45fbb2448690f2f4e8b
Yousefbahr <yousef.tbahr@gmail.com>
2026-02-26 07:15:10 +0200

    prepared for semi-prod

--- backend/app/services/translation_service.py
+++ backend/app/services/translation_service.py
@@ -2,7 +2,7 @@ from io import BytesIO
 import requests
 from app.config.config import *
 from app.core.prompts import *
-# from app.services.extract_text import *
+from app.services.extract_text import *
 from app.core.graph_models import *
 from app.core.workflow import graph
 from app.core.simple_calls import *
@@ -27,7 +27,7 @@ def translate_text(text: str, prev_text: str, source_lang: str, target_lang: str
         source_text=text,
         source_lang=source_lang,
         target_lang=target_lang,
-        max_iterations=1,
+        max_iterations=3,
         prev_context=prev_text)
 
     try:
@@ -130,108 +130,108 @@ def translate_file_content_pdf_streaming(pdf_bytes: bytes, source_lang: str, tar
 #    196.09584045410156,
 #    695.9491577148438]}
 #  ]]
-    content = [
-    [
-        {
-            "text": (
-                "In a quiet village tucked between two silver-blue mountains lived an old clockmaker\n"
-                "named Yarek. His shop was small—barely more than a room with dusty windows and\n"
-                "wooden shelves—but inside it, time itself seemed to rest. Clocks of every kind ticked and\n"
-                "chimed: brass pocket watches, tall pendulum clocks, tiny cuckoos perched in their carved\n"
-                "houses. Yet Yarek's most cherished project was an unfinished clock lying open on his work-\n"
-                "bench, its gears glinting like tiny suns.\n"
-            ),
-            "bbox": [71.0361557006836, 212.41976928710938, 541.9181518554688, 314.9400634765625],
-        },
-        {
-            "text": (
-                "Every morning, a girl named Lina passed by on her way to school. She would stop at the\n"
-                "door, press her face to the glass, and watch Yarek work with his steady hands. One foggy\n"
-                "morning, Yarek waved her inside.\n"
-            ),
-            "bbox": [70.59217834472656, 334.8522644042969, 542.0496826171875, 383.2663269042969],
-        },
-        {
-            "text": (
-                "For weeks, Lina visited the shop. She learned to oil tiny screws, polish brass, and set\n"
-                "springs just right. Slowly, the old clockmaker's hands grew steadier when hers were beside\n"
-                "them. One afternoon as winter crept closer. Yarek placed the Promise Clock in front of her.\n"
-            ),
-            "bbox": [70.64075469970703, 611.88818359375, 542.0708618164062, 662.1046142578125],
-        },
-        {
-            "text": (
-                "Lina nodded. \"Everyone says you can fix any clock—but that one,\" she said, pointing to\n"
-                "the brass clock on his bench, \"never moves.\"\n"
-            ),
-            "bbox": [71.4128189086914, 439.0286865234375, 541.7099609375, 470.2324523925781],
-        },
-        {
-            "text": (
-                "Yarek smiled softly. \"That one is special. It's the Promise Clock. I've been building it\n"
-                "for someone who hasn't arrived yet.\"\n"
-            ),
-            "bbox": [71.08402252197266, 490.76544189453125, 541.5205688476562, 522.9660034179688],
-        },
-        {
-            "text": 'Who?"she asked.\n',
-            "bbox": [89.61292266845703, 542.5999145507812, 188.7371063232422, 557.3048706054688],
-        },
-        {
-            "text": (
-                "\"I don't know.\" Yarek said. \"But I'll know when they come.\"\n"
-            ),
-            "bbox": [89.64185333251953, 576.43896484375, 405.8104248046875, 591.8262939453125],
-        },
-        {
-            "text": "The Clockmaker's Promise\n",
-            "bbox": [194.03944396972656, 119.21239471435547, 418.2802429199219, 140.8619384765625],
-        },
-        {
-            "text": '"Curious about time, are you?”he asked.\n',
-            "bbox": [89.80379486083984, 403.671630859375, 304.6008605957031, 418.70947265625],
-        },
-        {
-            "text": '"It\'s yours,” he said.\n',
-            "bbox": [89.6241455078125, 680.7218017578125, 196.09584045410156, 695.9491577148438],
-        },
-    ],
-    [
-        {
-            "text": (
-                "And from that day on, the clockmaker and the girl built not just clocks, but the gentle\n"
-                "rhythm of a friendship that echoed long after the mountains swallowed the sun.\n"
-            ),
-            "bbox": [70.90166473388672, 264.0069580078125, 541.6506958007812, 295.7639465332031],
-        },
-        {
-            "text": (
-                "She wound the clock with trembling fingers. For the first time, the gears stirred. A small,\n"
-                "clear tick filled the room — the beginning of something steady and true.\n"
-            ),
-            "bbox": [70.33451843261719, 160.23959350585938, 541.2111206054688, 191.78781127929688],
-        },
-        {
-            "text": (
-                "\"I told you,\" Yarek said, \"I was waiting for the right person. Someone who listens to the\n"
-                "quiet things.\"\n"
-            ),
-            "bbox": [70.48062896728516, 108.12611389160156, 541.4752197265625, 139.87916564941406],
-        },
-        {
-            "text": (
-                "Yarek placed a hand over hers. \"Time is precious,\" he said. \"But it's even more precious\n"
-                "when shared.\"\n"
-            ),
-            "bbox": [70.39310455322266, 211.81625366210938, 541.777099609375, 243.21131896972656],
-        },
-        {
-            "text": 'Lina blinked. "But I didn\'t order it."\n',
-            "bbox": [88.10195922851562, 73.16705322265625, 280.9306335449219, 87.18816375732422],
-        },
-    ],
-]
-    # content = extract_text_from_pdf(pdf_bytes)
+#     content = [
+#     [
+#         {
+#             "text": (
+#                 "In a quiet village tucked between two silver-blue mountains lived an old clockmaker\n"
+#                 "named Yarek. His shop was small—barely more than a room with dusty windows and\n"
+#                 "wooden shelves—but inside it, time itself seemed to rest. Clocks of every kind ticked and\n"
+#                 "chimed: brass pocket watches, tall pendulum clocks, tiny cuckoos perched in their carved\n"
+#                 "houses. Yet Yarek's most cherished project was an unfinished clock lying open on his work-\n"
+#                 "bench, its gears glinting like tiny suns.\n"
+#             ),
+#             "bbox": [71.0361557006836, 212.41976928710938, 541.9181518554688, 314.9400634765625],
+#         },
+#         {
+#             "text": (
+#                 "Every morning, a girl named Lina passed by on her way to school. She would stop at the\n"
+#                 "door, press her face to the glass, and watch Yarek work with his steady hands. One foggy\n"
+#                 "morning, Yarek waved her inside.\n"
+#             ),
+#             "bbox": [70.59217834472656, 334.8522644042969, 542.0496826171875, 383.2663269042969],
+#         },
+#         {
+#             "text": (
+#                 "For weeks, Lina visited the shop. She learned to oil tiny screws, polish brass, and set\n"
+#                 "springs just right. Slowly, the old clockmaker's hands grew steadier when hers were beside\n"
+#                 "them. One afternoon as winter crept closer. Yarek placed the Promise Clock in front of her.\n"
+#             ),
+#             "bbox": [70.64075469970703, 611.88818359375, 542.0708618164062, 662.1046142578125],
+#         },
+#         {
+#             "text": (
+#                 "Lina nodded. \"Everyone says you can fix any clock—but that one,\" she said, pointing to\n"
+#                 "the brass clock on his bench, \"never moves.\"\n"
+#             ),
+#             "bbox": [71.4128189086914, 439.0286865234375, 541.7099609375, 470.2324523925781],
+#         },
+#         {
+#             "text": (
+#                 "Yarek smiled softly. \"That one is special. It's the Promise Clock. I've been building it\n"
+#                 "for someone who hasn't arrived yet.\"\n"
+#             ),
+#             "bbox": [71.08402252197266, 490.76544189453125, 541.5205688476562, 522.9660034179688],
+#         },
+#         {
+#             "text": 'Who?"she asked.\n',
+#             "bbox": [89.61292266845703, 542.5999145507812, 188.7371063232422, 557.3048706054688],
+#         },
+#         {
+#             "text": (
+#                 "\"I don't know.\" Yarek said. \"But I'll know when they come.\"\n"
+#             ),
+#             "bbox": [89.64185333251953, 576.43896484375, 405.8104248046875, 591.8262939453125],
+#         },
+#         {
+#             "text": "The Clockmaker's Promise\n",
+#             "bbox": [194.03944396972656, 119.21239471435547, 418.2802429199219, 140.8619384765625],
+#         },
+#         {
+#             "text": '"Curious about time, are you?”he asked.\n',
+#             "bbox": [89.80379486083984, 403.671630859375, 304.6008605957031, 418.70947265625],
+#         },
+#         {
+#             "text": '"It\'s yours,” he said.\n',
+#             "bbox": [89.6241455078125, 680.7218017578125, 196.09584045410156, 695.9491577148438],
+#         },
+#     ],
+#     [
+#         {
+#             "text": (
+#                 "And from that day on, the clockmaker and the girl built not just clocks, but the gentle\n"
+#                 "rhythm of a friendship that echoed long after the mountains swallowed the sun.\n"
+#             ),
+#             "bbox": [70.90166473388672, 264.0069580078125, 541.6506958007812, 295.7639465332031],
+#         },
+#         {
+#             "text": (
+#                 "She wound the clock with trembling fingers. For the first time, the gears stirred. A small,\n"
+#                 "clear tick filled the room — the beginning of something steady and true.\n"
+#             ),
+#             "bbox": [70.33451843261719, 160.23959350585938, 541.2111206054688, 191.78781127929688],
+#         },
+#         {
+#             "text": (
+#                 "\"I told you,\" Yarek said, \"I was waiting for the right person. Someone who listens to the\n"
+#                 "quiet things.\"\n"
+#             ),
+#             "bbox": [70.48062896728516, 108.12611389160156, 541.4752197265625, 139.87916564941406],
+#         },
+#         {
+#             "text": (
+#                 "Yarek placed a hand over hers. \"Time is precious,\" he said. \"But it's even more precious\n"
+#                 "when shared.\"\n"
+#             ),
+#             "bbox": [70.39310455322266, 211.81625366210938, 541.777099609375, 243.21131896972656],
+#         },
+#         {
+#             "text": 'Lina blinked. "But I didn\'t order it."\n',
+#             "bbox": [88.10195922851562, 73.16705322265625, 280.9306335449219, 87.18816375732422],
+#         },
+#     ],
+# ]
+    content = extract_text_from_pdf(pdf_bytes)
     # print(content)
     
     # the following two lines are only here to return reading order of the pdf
