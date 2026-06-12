f1efe07efd952b3cb249312bd44184374d644aea
Yousefbahr <yousef.tbahr@gmail.com>
2026-06-13 00:21:34 +0300

    changed local host from 8001 to 8000

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,7 +1,7 @@
 // src/apiConfig.js
 
 // while using local host use this url, instead with tunneling use an emtpy string
-// export const API_URL = 'http://localhost:8001';
+// export const API_URL = 'http://localhost:8000';
 
 // with tunneling
 export const API_URL = '';
