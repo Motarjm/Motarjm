108245155f88d7035b4c5cac32c22c506e15a7a6
Yousefbahr <yousef.tbahr@gmail.com>
2026-06-16 23:13:54 +0300

    using gemini 3 flash for translation

--- backend/app/core/llms.py
+++ backend/app/core/llms.py
@@ -75,7 +75,7 @@ gpt_5_nano  = ChatOpenAI(
 )
 
 
-providers = {"translator": [claude_haiku_4_5,
+providers = {"translator": [gemini_3_flash_prev,
                             deepseek],
                                 # deepseek,
              
