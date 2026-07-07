3d6caa3a27eef3fc276d138c3ed27dd5d573bda5
Yousefbahr <yousef.tbahr@gmail.com>
2026-07-07 19:34:40 +0300

    adding exa api key in config

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -12,7 +12,7 @@ os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 os.environ["LANGSMITH_TRACING"] = os.getenv("LANGSMITH_TRACING")
 os.environ["LANGSMITH_PROJECT"] = "Turjman"
 os.environ["LANGSMITH_ENDPOINT"] = "https://eu.api.smith.langchain.com"
-
+os.environ["EXA_API_KEY"] = os.getenv("EXA_API_KEY")
 
 
 
